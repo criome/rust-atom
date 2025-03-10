3de62e794f4b6b2597a3f6ef1050711a9a70d24c
li <li@maisiliym.criome.me>
2025-03-10 11:27:43 +0100

    added(support for rust-toolchains.toml with rust-overlay)

--- flake.nix
+++ flake.nix
@@ -11,6 +11,9 @@
     nixpkgs-atom.inputs.nixpkgs.follows = "nixpkgs";
 
     crane.url = "github:ipetkov/crane";
+
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs: inputs.atom.mkAtomicFlake inputs (./. + "/rust-atom@.toml");
--- rust-atom/Make.nix
+++ rust-atom/Make.nix
@@ -5,9 +5,29 @@
   ...
 }:
 let
+  rustToolchainFilePath = (src + "/rust-toolchain.toml");
+  hasToolchainFile = std.pathExists rustToolchainFilePath;
+
+  rust-bin = get.rust-overlay.lib.mkRustBin { } pkgs;
+
+  # Modified version of rust-overlay's fromRustupToolchainTomlFile which only supports
+  # the toml rust-toolchain file - Send upstream?
+  fromRustupToolchainTomlFile =
+    path:
+    let
+      fileData = std.fromTOML (std.readFile path);
+    in
+    rust-bin.fromRustupToolchain fileData.toolchain;
+
+  customToolchain = fromRustupToolchainTomlFile rustToolchainFilePath;
+
+  customCraneLib = crane-lib.overrideToolchain customToolchain;
+
+  finalCraneLib = if hasToolchainFile then customCraneLib else crane-lib;
+
   args = { inherit src; };
 
-  cargoArtifacts = crane-lib.buildDepsOnly args;
+  cargoArtifacts = finalCraneLib.buildDepsOnly args;
 
   buildArgs = args // {
     inherit cargoArtifacts;
@@ -16,5 +36,5 @@ let
 in
 {
   inherit cargoArtifacts;
-  package = crane-lib.buildPackage buildArgs;
+  package = finalCraneLib.buildPackage buildArgs;
 }
