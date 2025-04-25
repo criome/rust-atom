b0a5ee7335c597eabc6bf6bf41018f82aa858ca1
li <li@maisiliym.criome.me>
2025-04-25 17:21:56 -0400

    adapt(upstream builtin pkgs and lib)

--- flake.nix
+++ flake.nix
@@ -3,16 +3,14 @@
 
   inputs = {
     make-atom.url = "github:criome/make-atom/testing";
+    make-atom.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     system.url = "github:criome/system";
 
-    nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
-    nixpkgs-lib.flake = false;
-
-    src.url = "github:superatomic/xshe";
+    src.url = "github:criome/horizon-rs/testing";
     src.flake = false;
-
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     crane.url = "github:ipetkov/crane";
 
@@ -20,21 +18,5 @@
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs =
-    inputs:
-    inputs.make-atom.mkAtom {
-      args.atomSrc = ./.;
-      args.atomName = "rust-atom";
-
-      system = inputs.system.value;
-
-      local-registry = {
-        inherit (inputs)
-          nixpkgs
-          nixpkgs-lib
-          crane
-          rust-overlay
-          ;
-      };
-    };
+  outputs = inputs: inputs.make-atom.mkAtomFlake ./. inputs;
 }
